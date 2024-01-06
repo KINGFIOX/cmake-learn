a6b5fdce6a8e7da6c83a5cc2496318d971b7405f
Wong Silence <wangfiox@hotmail.com>
2024-01-06 20:16:16 +0800

    modified:   .gitignore 	new file:   chap5/508version/CMakeLists.txt 	new file:   chap5/508version/slib.cc 	new file:   chap5/508version/slib.cc.in 	new file:   chap5/508version/slib.cpp 	new file:   chap5/509version/CMakeLists.txt 	new file:   chap5/509version/main.cc

---
+++ chap5/508version/slib.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "slib.h"  
+void SLib(){
+std::cout << "In SLib function!\n";
+}
---
+++ chap5/509version/main.cc
@@ -0,0 +1,8 @@
+#include "slib.h"
+#include <iostream>
+using namespace std;
+int main(void){
+    cout << "in main" << endl;
+    SLib();
+    return 0;
+}
